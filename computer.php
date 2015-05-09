50bb3d177c81ec13f77b806ad3362cc403191c49
rafanwij <rafanwij@gmail.com>
2015-05-09 14:18:16 +0700

    computer
    form computer

---
+++ computer.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Computer</title>
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<!-- Static navbar -->
+    <nav class="navbar navbar-default navbar-static-top" style="background-color:#13244f;color:#f9494b">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <!-- ini kasih logo nya sama tulisan coba disamain yaa -->
+          <a class="navbar-brand" href="index.php">Vonrais</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="computer.php">Computer</a></li>
+            <li><a href="forum.php">Forum</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <form class="form-inline" style="margin-top:0.5em;">
+              <div class="form-group">
+                <label class="sr-only" for="searchBar">Search Product</label>
+                <div class="input-group">
+                  <div class="input-group-addon"><span class="glyphicon glyphicon-search"></span></div>
+                  <input type="text" class="form-control" id="searchBar" placeholder="Search Product">
+                </div>
+              </div>
+              <button type="submit" class="btn btn-primary">Search</button>
+            </form>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+	<form>
+		<div class="panel panel-info" style="margin:10px">
+			<div class="panel-heading" align="center">VONRAISComp</div>
+			<div class="panel-body">
+
+			</div>
+		</div>
+	</form>
+</body>
+</html>
